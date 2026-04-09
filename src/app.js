09f7efab330c7f2ecbfcb0c836ffefba77127c80
Facundo Yamil R <facundoyamilrivero@gmail.com>
2026-04-09 19:50:02 -0300

    commit antes de ir a via cosenza

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import cors from "cors";
+
 import express from "express";
 
 import user_Routes from "./routes/user.routes.js";
@@ -7,6 +9,7 @@ import products_Routes from "./routes/product.routes.js";
 // crear la aplicacion de expresss
 const app = express(); 
 
+app.use(cors()) 
 // middlewares para parsear el cuerpo de las solicitudes y manejar datos en formato JSON, esto es necesario para poder recibir datos en el cuerpo de las peticiones POST y PUT, por ejemplo cuando queremos crear o actualizar un usuario
 app.use(express.json())
 
