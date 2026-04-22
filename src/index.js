d6dfc09dc517baca95bea404ab6497108c36da12
Facundo Yamil R <facundoyamilrivero@gmail.com>
2026-04-22 11:24:01 -0300

    chore: forzando nueva estructura sin lockfile y con dependencias full

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000; 
 
-console.log("🚀 Intentando arrancar el motor...");
+console.log(" Intentando arrancar el motor...");
 
 
-console.log("🔗 La URI que estoy usando es:", process.env.MONGO_URI);
+console.log(" La URI que estoy usando es:", process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
-    console.log("✅ Conexión exitosa a MongoDB");
+    console.log(" Conexión exitosa a MongoDB");
 
     app.listen(PORT, () => {
         
-        console.log(`📡 Servidor escuchando en el puerto: ${PORT}`);
+        console.log(` Servidor escuchando en el puerto: ${PORT}`);
     });
 })
 .catch((error) => {
-    console.error("❌ ERROR CRÍTICO AL CONECTAR:", error.message);
+    console.error("ERROR CRÍTICO AL CONECTAR:", error.message);
     process.exit(1); 
 });
